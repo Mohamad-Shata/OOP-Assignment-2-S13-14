3ba72262770348ea5bd44b89567cf92560c55dbf
Mohamad-Shata <146951820+Mohamad-Shata@users.noreply.github.com>
2023-11-04 16:53:49 +0200

    Update draft.cpp

--- draft.cpp
+++ draft.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -16,26 +17,26 @@ private:
             return false;
         }
 
-        // Check if the string starts with a valid sign (+ or -)
-        if (realNumber[0] != '+' && realNumber[0] != '-' && !isdigit(realNumber[0])) {
-            return false;
-        }
-
-        // Check if the string contains more than one decimal point
-        int decimalCount = 0;
+        // Check if the string contains only valid characters
         for (char c : realNumber) {
-            if (c == '.') {
-                decimalCount++;
-                if (decimalCount > 1) {
-                    return false;
-                }
-            } else if (!isdigit(c) && c != '+' && c != '-') {
+            if (!isdigit(c) && c != '.' && c != '+' && c != '-') {
                 return false;
             }
         }
 
-        // Check if the string ends with a digit
-        if (!isdigit(realNumber.back())) {
+        // Check if the string has a valid sign
+        if (realNumber[0] != '+' && realNumber[0] != '-' && !isdigit(realNumber[0])) {
+            return false;
+        }
+
+        // Check if the string has a valid decimal point
+        int decimalCount = 0;
+        for (char c : realNumber) {
+            if (c == '.') {
+                decimalCount++;
+            }
+        }
+        if (decimalCount > 1) {
             return false;
         }
 
@@ -43,69 +44,207 @@ private:
     }
 
 public:
-    BigReal(string realNumber) {
-        if (isValidReal(realNumber)) {
-            if (realNumber[0] == '+' || realNumber[0] == '-') {
-                sign = realNumber[0];
-                realNumber.erase(0,1);
-                if (realNumber.find('.') != string::npos) {
-                    intpart = realNumber.substr(0, realNumber.find('.'));
-                    fractionpart = realNumber.substr(realNumber.find('.') + 1);
-                } else {
-                    intpart = realNumber;
-                }
-            } else {
-                if (realNumber.find('.') != string::npos) {
-                    intpart = realNumber.substr(0, realNumber.find('.'));
-                    fractionpart = realNumber.substr(realNumber.find('.') + 1);
-                } else {
-                    intpart = realNumber;
-                }
-            }
+    BigReal(const string& realNumber) {
+        if (!isValidReal(realNumber)) {
+            throw invalid_argument("Invalid real number");
+        }
+
+        // Initialize the sign, intpart, and fractionpart
+        sign = (realNumber[0] == '-') ? "-" : "+";
+        size_t decimalPos = realNumber.find('.');
+        if (decimalPos != string::npos) {
+            intpart = realNumber.substr(0, decimalPos);
+            fractionpart = realNumber.substr(decimalPos + 1);
         } else {
-            intpart = realNumber;
+            intpart = realNumber.substr(0);
+            fractionpart = "";
+        }
+        if (realNumber[0] == '+' || realNumber[0] == '-') {
+            intpart.erase(0,1);
+        }
+        // Remove leading zeros from intpart
+        size_t firstNonZero = intpart.find_first_not_of('0');
+        if (firstNonZero != string::npos) {
+            intpart = intpart.substr(firstNonZero);
+        }
+
+        // Remove trailing zeros from fractionpart
+        size_t lastNonZero = fractionpart.find_last_not_of('0');
+        if (lastNonZero != string::npos) {
+            fractionpart = fractionpart.substr(0, lastNonZero + 1);
+        }
+
+        // Check if the number is zero
+        if (intpart.empty() && fractionpart.empty()) {
+            sign = "+";
+        }
+
+        // Construct the number string
+        number = sign + intpart + "." + fractionpart;
+    }
+bool findMaxValue(const std::string& str1, const std::string& str2) const {
+    if (str1.length() > str2.length()) {
+        return true;
+    } else if (str1.length() < str2.length()) {
+        return false;
+    } else {
+        for (size_t i = 0; i < str1.length(); ++i) {
+            if (str1[i] > str2[i]) {
+                return true;
+            } else if (str1[i] < str2[i]) {
+                return false;
+            }
+        }
+    }
+    return false;
+}
+
+
+
+    
+
+    BigReal operator+(const BigReal& other) const {
+        BigReal result("0");
+
+        // variables used
+        string fraction1 = fractionpart;
+        string fraction2 = other.fractionpart;
+        int sizeDiff = fraction1.size() - fraction2.size();
+        string integer1 = intpart;
+        string integer2 = other.intpart;
+        int intsizeDiff = integer1.size() - integer2.size();
+
+        if (intsizeDiff > 0) {
+
+        for(int i = intsizeDiff ; i != 0 ; i--){
+
+            integer2 = '0' + integer2;
+
+            }
+        } else if (intsizeDiff < 0) {
+
+        for(int i = intsizeDiff ; i != 0 ; i++){
+
+            integer1 = '0' + integer1;
+            }  
+        }
+
+        if (sizeDiff > 0) {
+
+            fraction2.insert(0 , sizeDiff , '0');
+
+        } else if (sizeDiff < 0) {
+
+            fraction1.resize(fraction2.size(),'0');
+        }
+
+        // addition of fraction part
+        if(sign == other.sign){
+        int carry = 0;
+        for (int i = fraction1.length() - 1; i >= 0; i--) {
+            int digit1 = fraction1[i] - '0';
+            int digit2 = fraction2[i] - '0';
+            int sum = digit1 + digit2 + carry;
+            result.fractionpart = to_string(sum % 10) + result.fractionpart;
+            carry = sum / 10;
+        }            
+
+
+        // Add any remaining carry
+            result.intpart = addWithCarry(integer1, integer2, carry);
+
+            result.number = sign + result.intpart + '.' + result.fractionpart;
+
+        return result;
+        }else{
+            if(findMaxValue(integer1, integer2)){
+        int borrow = 0;
+        for (int i = fraction1.length() - 1; i >= 0; i--) {
+            int digit1 = fraction1[i] - '0';
+            int digit2 = fraction2[i] - '0';
+            int diff = digit1 - digit2 - borrow;
+            if (diff < 0) {
+                diff += 10;
+                borrow = 1;
+            } else {
+                borrow = 0;
+            }
+            result.fractionpart = to_string(diff) + result.fractionpart;
+        }
+        
+        result.intpart = subtractWithBorrow(integer1 , integer2 , borrow);
+
+        result.number = sign + result.intpart + '.' + result.fractionpart;
+            }else if(!findMaxValue( integer1 , integer2)){
+        int borrow = 0;
+        for (int i = fraction1.length() - 1; i >= 0; i--) {
+            int digit1 = fraction1[i] - '0';
+            int digit2 = fraction2[i] - '0';
+            int diff = digit1 - digit2 - borrow;
+            if (diff < 0) {
+                diff += 10;
+                borrow = 1;
+            } else {
+                borrow = 0;
+            }
+            result.fractionpart = to_string(diff) + result.fractionpart;
+        }
+        result.intpart = subtractWithBorrow(integer2 , integer1 , borrow);
+
+        result.number = other.sign + result.intpart + '.' + result.fractionpart;
+            }else{
+                result.number = '0';
+            }
+            return result;
         }
     }
 
-BigReal operator+(const BigReal& other) const {
-    BigReal result("0");
+    string addWithCarry(const string& num1, const string& num2, int carry) const {
+        string result;
 
-    // Perform addition on the integer parts
-    double sum = stod(intpart) + stod(other.intpart);
-    result.intpart = to_string(sum);
+        for (int i = num1.length() - 1 ; i >= 0 ; i--) {
+            int digit1 = num1[i] - '0';
+            int digit2 = num2[i] - '0';
+            int sum = digit1 + digit2 + carry;
+            result = to_string(sum % 10) + result;
+            carry = sum / 10;
+        }
 
-    // Perform addition on the fraction parts
-    string fraction1 = fractionpart;
-    string fraction2 = other.fractionpart;
-    int sizeDiff = fraction1.size() - fraction2.size();
-
-    if (sizeDiff > 0) {
-        // Pad fraction2 with zeros
-        fraction2.insert(0, sizeDiff, '0');
-    } else if (sizeDiff < 0) {
-        // Pad fraction1 with zeros
-        fraction1.insert(0, -sizeDiff, '0');
+        return result;
+    
     }
 
-    int carry = 0;
-    for (int i = fraction1.length() - 1; i >= 0; i--) {
-        int digit1 = fraction1[i] - '0';
-        int digit2 = fraction2[i] - '0';
-        int sum = digit1 + digit2 + carry;
-        result.fractionpart = to_string(sum % 10) + result.fractionpart;
-        carry = sum / 10;
-    }
+string subtractWithBorrow(const string& num1, const string& num2, int borrow) const {
+    string result;
+    int n = num1.length();
 
-    // Add any remaining carry
-    if (carry > 0) {
-        result.intpart = to_string(stod(result.intpart) + carry);
+    for (int i = n - 1; i >= 0; i--) {
+        int digit1 = num1[i] - '0';
+        int digit2 = num2[i] - '0';
+
+        // Apply borrow
+        digit1 -= borrow;
+
+        // Perform subtraction
+        int diff = digit1 - digit2;
+
+        // Update borrow for the next iteration
+        if (diff < 0) {
+            diff += 10;
+            borrow = 1;
+        } else {
+            borrow = 0;
+        }
+
+        // Convert the difference to a character and prepend it to the result
+        result = to_string(diff) + result;
     }
 
     return result;
 }
 
 
-    BigReal operator-(const BigReal& other) const {
+        BigReal operator-(const BigReal& other) const {
         BigReal result("0");
 
         // Perform subtraction on the integer parts
@@ -146,63 +285,19 @@ BigReal operator+(const BigReal& other) const {
 
         return result;
     }
-
-    bool operator<(const BigReal& anotherReal) const {
-        if (sign != anotherReal.sign) {
-            if (sign == "-") return true;
-            else if (sign == "+") return false;
-        }
-
-        if (intpart.size() < anotherReal.intpart.size()) return true;
-        else if (intpart.size() > anotherReal.intpart.size()) return false;
-        else {
-            for (int i = 0; i < intpart.size(); i++) {
-                if (intpart[i] < anotherReal.intpart[i]) return true;
-                else if (intpart[i] > anotherReal.intpart[i]) return false;
-            }
-        }
-
-        string fraction1 = fractionpart;
-        string fraction2 = anotherReal.fractionpart;
-        if (fraction1.size() > fraction2.size()) {
-            while (fraction2.size() < fraction1.size()) {
-                fraction2.push_back('0');
-            }
-        } else if (fraction1.size() < fraction2.size()) {
-            while (fraction1.size() < fraction2.size()) {
-                fraction1.push_back('0');
-            }
-        }
-
-        for (int i = 0; i < fraction1.size(); i++) {
-            if (fraction1[i] < fraction2[i]) return true;
-            else if (fraction1[i] > fraction2[i]) return false;
-        }
-
-        return false;
+    void print() const {
+        cout << number << endl;
     }
-void print() const {
-    cout << intpart;
-    if (!fractionpart.empty()) {
-        // Remove trailing zeros from fractionpart
-        size_t lastNonZero = fractionpart.find_last_not_of('0');
-        if (lastNonZero != string::npos) {
-            cout << fractionpart.substr(0, lastNonZero + 1);
-        }
-    }
-    cout << endl;
-}
-
-    friend ostream& operator<<(ostream& out, const BigReal& num) {
-        out << num.number;
-        return out;
+    void test(){
+        cout << intpart << endl << fractionpart << endl;
     }
 };
 
 int main() {
-    BigReal n1("11.91");
-    BigReal n2("2333333333339.1134322222222292");
+    BigReal n1("-111.0");
+    BigReal n2("1.5");
     BigReal n3 = n1 + n2;
+
     n3.print();
 
     return 0;
