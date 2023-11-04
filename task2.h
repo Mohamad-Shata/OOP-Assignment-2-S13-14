d69db3f1a1e05ec8f5a6e6964a8ac6b87faf3f73
Mohamad-Shata <146951820+Mohamad-Shata@users.noreply.github.com>
2023-11-04 19:49:51 +0200

    Update task2.h

--- task2.h
+++ task2.h
@@ -1,14 +1,40 @@
-//
-// Created by Hp on 10/30/2023.
-//
+#ifndef BIGREAL_H
+#define BIGREAL_H
 
-#ifndef OOP_ASSIGNMENT_2_S13_14_TASK2_H
-#define OOP_ASSIGNMENT_2_S13_14_TASK2_H
+#include <iostream>
+#include <string>
 
+using namespace std;
 
-class task2 {
+class BigReal {
+private:
+    string number;
+    string intpart;
+    string fractionpart;
+    string sign;
 
+    bool isValidReal(const string& realNumber);
+
+    string addZerosToLeft(const string& number, int numZeros);
+
+    string addWithCarry(const string& num1, const string& num2, int& carry) const;
+
+    string subtractWithBorrow(const string& num1, const string& num2, int borrow) const;
+
+    bool findMaxValue(const string& num1, const string& num2) const;
+
+public:
+    BigReal(const string& realNumber);
+
+    BigReal operator+(const BigReal& other) const;
+
+    bool operator<(const BigReal& other) const;
+
+    bool operator>(const BigReal& other) const;
+
+    bool operator==(const BigReal& other) const;
+
+    friend ostream& operator<<(ostream& out, const BigReal& num);
 };
 
-
-#endif //OOP_ASSIGNMENT_2_S13_14_TASK2_H
+#endif // BIGREAL_H
