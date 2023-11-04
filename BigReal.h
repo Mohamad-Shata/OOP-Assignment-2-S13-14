389e6f9d791b073ce56601ceb4f01196c11feefd
Mohamad-Shata <146951820+Mohamad-Shata@users.noreply.github.com>
2023-11-04 19:52:51 +0200

    Update BigReal.h

--- BigReal.h
+++ BigReal.h
@@ -7,7 +7,47 @@ using namespace std;
 class BigReal {
 private:
     string number;
+    string intpart;#ifndef BIGREAL_H
+#define BIGREAL_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BigReal {
+private:
+    string number;
     string intpart;
+    string fractionpart;
+    string sign;
+
+    bool isValidReal(const string& realNumber);
+
+    string addZerosToLeft(const string& number, int numZeros);
+
+    string addWithCarry(const string& num1, const string& num2, int& carry) const;
+
+    string subtractWithBorrow(const string& num1, const string& num2, int borrow) const;
+
+    bool findMaxValue(const string& num1, const string& num2) const;
+
+public:
+    BigReal(const string& realNumber);
+
+    BigReal operator+(const BigReal& other) const;
+
+    bool operator<(const BigReal& other) const;
+
+    bool operator>(const BigReal& other) const;
+
+    bool operator==(const BigReal& other) const;
+
+    friend ostream& operator<<(ostream& out, const BigReal& num);
+};
+
+#endif // BIGREAL_H
+
     string fractionpart;
     string sign;
 
